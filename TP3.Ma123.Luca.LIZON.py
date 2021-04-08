15c2a26768a55679ad148040bf59523bbfccf95e
LucaLizon <82183575+LucaLizon@users.noreply.github.com>
2021-04-08 23:59:24 +0200

    Add files via upload

---
+++ TP3.Ma123.Luca.LIZON.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  8 22:46:18 2021
+
+@author: lucal
+"""
+
+import fonctions3 as f3
+
+def Newton(f,fd,x0,epsilon,Nitermax):
+    n=0
+    xold = x0
+    erreur = f(xold) - xold
+    while abs(erreur)> epsilon and n<Nitermax:
+        xnew = xold-(f(xold)/fd(xold))
+        erreur = xnew - xold
+        xold = xnew
+        print(n)
+        n+=1
+    return xnew 
+
+print(Newton(f3.f1,f3.f1der,10**-7,-10,30))
